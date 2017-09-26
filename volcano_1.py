a3cea9a77b731ae4f18547f984afccf4035b65f6
Frank Sedlar <frank.sedlar@gza.com>
2017-09-26 19:42:01 -0400

    Initial commit

---
+++ volcano_1.py
@@ -0,0 +1,24 @@
+'''
+Indonesian Volcano visualization project
+Data from the Smithsonian Global Volcanism Program
+http://volcano.si.edu/database/search_eruption_results.cfm#
+
+NOAA also has data
+https://catalog.data.gov/dataset/global-significant-volcanic-eruptions-database-4360-bc-to-present
+
+1. Function to write the excel file into a postgres db
+2. Set up a NodeJS Project
+3. Initialize basic Koa server
+4. Add basic postgres integration
+5. Add Geojson endpoints
+6. Advanced postgis queries
+7. Integreate Redis
+8. Leaflet, flask, postgis 
+
+
+
+
+'''
+
+
+
